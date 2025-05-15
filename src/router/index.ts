71bd33ad8f752e4b70fd08ff00e4ffff382ea800
Cristian David Home <cristian_david033@hotmail.com>
2025-05-15 06:34:10 -0500

    feat: :sparkles: Implement background music functionality and update game logo styling

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GamePlayView from '../views/GamePlayView.vue'
 import GameOverView from '../views/GameOverView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
