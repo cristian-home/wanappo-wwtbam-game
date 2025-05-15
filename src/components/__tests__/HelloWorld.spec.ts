893c9d26bc98d42e88db343f343012df0674667e
Cristian David Home <cristian_david033@hotmail.com>
2025-05-14 19:59:11 -0500

    fix: :bug: Update QuestionsPanel text and corresponding test expectation.

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -6,6 +6,6 @@ import QuestionsPanel from '../QuestionsPanel.vue'
 describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(QuestionsPanel, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Question Panel')
+    expect(wrapper.text()).toContain('Useless')
   })
 })
